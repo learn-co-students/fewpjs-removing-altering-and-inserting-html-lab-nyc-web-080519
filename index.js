854c4ed0d5525a331999de637216560ba6055017
github username <github email address>
2019-09-16 18:10:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+
+/* Enter the code to remove the node element here */
+document.getElementById("main").remove();
+ 
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
