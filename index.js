02e61ac04635727d48bd889f9aac29aa9874ebfe
github username <github email address>
2019-09-17 12:07:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// import { brotliDecompressSync } from "zlib";
+
+document.querySelector("main#main").remove();
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.className = "victory";
+newHeader.innerHTML = "Darrow is the champion!";
