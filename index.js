8dfda8b0dfd57325dbbdc0bde96ec39e615e5999
DanBehrman <dan.behrman@wework.com>
2019-09-17 20:08:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory")
+document.body.appendChild(newHeader);
+newHeader.innerHTML = 'Dan is the champion';
